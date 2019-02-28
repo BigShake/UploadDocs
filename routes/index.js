3d0c81bcddfb550fd17b68d767edccc35df6d3b9
shawanning <wanning.sha@ecovacs.com>
2019-02-28 17:30:31 +0800

    add new pro

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+module.exports = function (app) {
+  app.get('/', function (req, res) {
+    res.redirect('/posts')
+  })
+  app.use('/signup', require('./signup'))
+  app.use('/signin', require('./signin'))
+  app.use('/signout', require('./signout'))
+  app.use('/posts', require('./posts'))
+  app.use('/comments', require('./comments'))
+  app.use('/uploadDocs', require('./uploadFiles'))
+  // app.use 第一个参数是 views 目录下的ejs文件中的 href 指向入口，第二个参数是指向js文件
+
+  // 404 page
+  app.use(function (req, res) {
+    if (!res.headersSent) {
+      res.status(404).render('404')
+    }
+  })
+}
